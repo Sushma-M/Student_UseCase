ed2b8264d411ff50a64d966c743be8bc5f19f5b6
root <root@5a906301c4d8>
2016-09-20 09:22:51 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,11 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "defaultLanguage" : "en",
+  "displayName" : "Student_UseCase",
+  "homePage" : "Main",
+  "name" : "Student_UseCase",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
